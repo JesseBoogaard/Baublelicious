fafb606ba6fdf99dc5bdac551a616b83ad83cfc3
denbukki <denbukki@gmail.com>
2016-12-14 21:04:51 +0100

    clean for 1.11

--- src/main/java/com/baublelicious/Baublelicious.java
+++ src/main/java/com/baublelicious/Baublelicious.java
@@ -1,60 +1,20 @@
 package com.baublelicious;
 
-import com.baublelicious.config.Config;
-import com.baublelicious.network.NetworkRegister;
-import com.baublelicious.items.BaubleliciousItems;
-import com.baublelicious.network.GuiHandler;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ModInfo.MOD_ID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies = "required-after:Baubles@[1.0.1.4,);", guiFactory = ModInfo.GUI_FACTORY_CLASS)
-public class Baublelicious {
-  @Instance(ModInfo.MOD_ID)
-  public static Baublelicious instance;
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.NAME, version = ModInfo.VERSION)
 
-  @SidedProxy(clientSide = "com.baublelicious.client.ClientProxy", serverSide = "com.baublelicious.CommonProxy")
-  public static CommonProxy proxy;
+public class Baublelicious
+{
 
-  public static CreativeTabs tabBaublelicious = new CreativeTabs("Baublelicious") {
-    @Override
-    public Item getTabIconItem() {
-      return BaubleliciousItems.ItemNecklaceDiving;
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+       
     }
-  };
-
-  public static Logger log;
-
-  @EventHandler
-  public void preInit(FMLPreInitializationEvent event) {
-    Config.init(event.getSuggestedConfigurationFile());
-    log = event.getModLog();
-
-    proxy.registerRenderThings();
-
-    proxy.preInit(event);
-
-    NetworkRegister.registerMessages();
-    FMLCommonHandler.instance().bus().register(new Config());
-  }
-
-
-  @EventHandler
-  public void init(FMLInitializationEvent event) {
-    NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
-    proxy.init();
-  }
-
-  @EventHandler
-  public void load(FMLInitializationEvent event) {
-    proxy.registerRenderThings();
-  }
 }
